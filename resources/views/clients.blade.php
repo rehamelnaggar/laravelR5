9e7f3cc7025cdfc449e2b1864737bda05a4c31fd
rehamelnaggar <rehamelsayedelnaggar@gmail.com>
2024-05-21 13:01:24 +0200

    task7

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -10,44 +10,51 @@
 </head>
 <body>
   @include('includes.nav')
-<div class="container">
-  <h2>Clients Data</h2>
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Client Name</th>
-        <th>Phone</th>
-        <th>Email</th>
-        <th>Website</th>
-        <th>Active</th>
-        <th>Edit</th>
-        <th>Show</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($clients as $client)
-      <tr>
-        <td>{{ $client->clientName }}</td>
-        <td>{{ $client->phone }}</td>
-        <td>{{ $client->email }}</td>
-        <td>{{ $client->website }}</td>
-        <td>{{ $client->active ? "yes" : "no" }}</td>
-        <td><a href="{{ route('editClients',$client->id)}}">Edit</a></td>
-        <td><a href="{{ route('showClient',$client->id)}}">Show</a></td>
-        <td>
-        <form action="{{ route('delClient')}}" method="post">
-          @csrf
-          @method('Delete')
-            <input type="hidden" value="{{ $client->id }}" name="id">
-            <input type="submit" onclick="return confirm('Are you sure to delete?')" value="Delete">
-       </form>
-        </td>
-      </tr>
-      @endforeach
-
-    </tbody>
-  </table>
-</div>
-
+  <div class="container">
+    <h2>Clients Data</h2>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Client Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>Website</th>
+          <th>Image</th>
+          <th>Active</th>
+          <th>Edit</th>
+          <th>Show</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($clients as $client)
+        <tr>
+          <td>{{ $client->clientName }}</td>
+          <td>{{ $client->phone }}</td>
+          <td>{{ $client->email }}</td>
+          <td>{{ $client->website }}</td>
+          <td>
+            @if ($client->image)
+              <img src="{{ asset('assets/images/' . $client->image) }}" alt="Client Image" width="50">
+            @else
+              No Image
+            @endif
+          </td>
+          <td>{{ $client->active ? "Yes" : "No" }}</td>
+          <td><a href="{{ route('editClients', $client->id) }}" class="btn btn-warning">Edit</a></td>
+          <td><a href="{{ route('showClient', $client->id) }}" class="btn btn-info">Show</a></td>
+          <td>
+            <form action="{{ route('delClient') }}" method="post">
+              @csrf
+              @method('DELETE')
+              <input type="hidden" value="{{ $client->id }}" name="id">
+              <input type="submit" onclick="return confirm('Are you sure to delete?')" value="Delete" class="btn btn-danger">
+            </form>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
